8e7ac719f5854a955cd07f8e904b7312b41025ca
Justin W Soto <soto@pdx.edu>
2021-10-04 14:24:03 -0700

    changed everything from module to commonjs and added a skeleton of first test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,12 +1,18 @@
-import pool from '../lib/utils/pool.js';
-import setup from '../data/setup.js';
-import request from 'supertest';
-import app from '../lib/app.js';
+const pool = require('../lib/utils/pool.js');
+const setup = require ('../data/setup.js');
+const request = require ('supertest');
+const app = require ('../lib/app.js');
 
-describe('demo routes', () => {
+
+describe('crudish routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('should save an animal', () => {
+    const animalObject = {x};
+    return request(app) .post('/') .send(animalObject) .then((res) => {expect(res.body).toEqual({x})})
+  })
 
   afterAll(() => {
     pool.end();
--- data/setup.js
+++ data/setup.js
@@ -1,5 +1,4 @@
-import { promises as fs } from 'fs';
-
+const fs = require ('fs/promises');
 export default (pool) => {
   return fs.readFile('./sql/setup.sql', { encoding: 'utf-8' })
     .then(sql => pool.query(sql));
